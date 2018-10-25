8281b0918e1cf694a22cc5a265ddf82b89f48827
epsid <44061778+epsid@users.noreply.github.com>
2018-10-25 10:32:04 +0300

    Delete Calculator.java

--- ru/minnumber/Calculator.java
+++
@@ -1,8 +0,0 @@
-package ru.minnumber;
-
-class Calculator {
-    static long mem() {
-        Runtime runtime = Runtime.getRuntime();
-        return runtime.totalMemory() - runtime.freeMemory();
-    }
-}
